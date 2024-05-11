a656967b4710aebf6d27f4c7fa88647afddcff75
kaviya-vj <64571995+kaviya-vj@users.noreply.github.com>
2024-05-11 20:00:48 +0530

    Update Employee.java

--- Task-12_JavaCollectionAndPackage/src/com/example/hr/Employee.java
+++ Task-12_JavaCollectionAndPackage/src/com/example/hr/Employee.java
@@ -2,10 +2,15 @@ package com.example.hr;
 
 public class Employee {
 	
-	String name = "Kaviya";
-	int id = 1908;
-	double salary = 525000.60;
+	private String name;
+	private int id;
+	private double salary;
 	
+	public Employee(String name, int id, double salary) {
+		this.name=name;
+		this.id=id;
+		this.salary=salary;
+	}
 	public void printName() {
 		System.out.println("Employee name is "+ name);		
 	}
